f167e88fa464f1a259fba7e6e49a3ff697e44711
yuki-h29 <yuki.hirano29@gmail.com>
2023-07-13 22:13:31 +0900

    Initial Commit

---
+++ testRealmDB/testRealmDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRealmDBApp.swift
+//  testRealmDB
+//
+//  Created by 平野裕貴 on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct testRealmDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
